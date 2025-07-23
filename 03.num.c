8bc007700a24c0417d4c8eade3735aa8a4a96f3b
SeoDUk <dndnd0120@naver.com>
2025-07-24 00:47:13 +0900

    num 변수

--- 03.num.c
+++ 03.num.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
-    int num;
-    num=20;
-    printf("%d",num);
+    int num1, num2;
+    int num3=30, num4=40;
+
+    printf("num1 : %d, num2 : %d\n",num1,num2);
+    num1=10;
+    num2=20;
+
+    printf("num1 : %d, num2 : %d\n",num1,num2);
+    printf("num3 : %d, num4 : %d\n",num3,num4);
+    
+    system("pause");
     return 0;
 }
