8bc3ebad03d09a08f96bb056d22c8bd66fb55e62
SeoDUk <dndnd0120@naver.com>
2025-07-24 00:26:09 +0900

    연습문제 1번

---
+++ 02.ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("My name is donuguk.\nI'm %dyears old.\n",20);
+    printf("my home is %d-%d.\n",123,456);
+    system("pause");
+    return 0;
+}
