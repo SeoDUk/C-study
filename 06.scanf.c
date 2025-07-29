3dd8cd84e5b4d9a7fd160ac3f56f4c7597ceec09
SeoDUk <dndnd0120@naver.com>
2025-07-29 16:00:25 +0900

    scanf 연습

---
+++ 06.scanf.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("input num1, num1");
+    scanf("%d %d",&n1,&n2);
+    
+    printf("%d/%d=%d\n",n1,n2,n1/n2);
+    printf("~ = %d",n1%n2);
+
+    system("pause");
+    return 0;
+
+
+
+}
